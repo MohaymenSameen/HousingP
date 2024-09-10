6dd57013226eebe2c21813dca471fddca35c725f
Mohaymen Sameen <42969169+MohaymenSameen@users.noreply.github.com>
2024-09-10 12:24:12 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -53,7 +53,7 @@ const runPuppeteer = async (url) => {
 
     const htmlString = await page.content();
     const dom = new JSDOM(htmlString);
-    console.log('HTML Snippet:', htmlString.substring(0, 500)); 
+    console.log('HTML Snippet:', htmlString.substring(0, 5000)); 
     
     console.log('parsing pararius.com data');
     const result = dom.window.document.querySelectorAll('ul.search-list');
